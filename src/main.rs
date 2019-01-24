9f6183a6087ca0ee176ffa23f4df4e0066d147e5
CodeSandwich <igor.zuk@protonmail.com>
2019-01-24 14:46:22 +0100

    Add example for state and future in response, wrap server code in ServerHandler

--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,67 @@ extern crate serde;
 #[macro_use] extern crate serde_json;
 
 use actix_net::server::Server;
-use actix_web::{actix, server, App, HttpRequest, HttpResponse, Json, Query, Responder};
+use actix_web::{actix, server, App, FutureResponse, HttpResponse, Json, Query, Responder, State};
 use actix_web::actix::Addr;
-use actix_web::server::{HttpServer, PauseServer, ResumeServer, StopServer};
-use futures::Future;
+use actix_web::error::Error as ActixError;
+use actix_web::server::StopServer;
+use futures::{Async, Future, Poll};
 use std::io::{BufRead, stdin};
-use std::sync::mpsc::sync_channel;
+use std::sync::{Arc, mpsc::sync_channel, Mutex};
 use std::thread;
 
+type CounterState = Arc<Mutex<u64>>;
+
 fn main() {
-    let server_addr_1 = start_server();
-    for line in stdin().lock().lines() {
-        match line.as_ref().map(|s| s.as_ref()) {
-            Ok("pause 1") => println!("{:?}", server_addr_1.send(PauseServer).wait()),
-            Ok("resume 1") => println!("{:?}", server_addr_1.send(ResumeServer).wait()),
-            Ok("stop 1") => println!("{:?}", server_addr_1.send(StopServer { graceful: true }).wait()),
-            _ => println!("Unknown command"),
-        }
+    let state = Arc::new(Mutex::new(0));
+    let server_handler = ServerHandler::start_server(state);
+    stdin()
+        .lock()
+        .lines()
+        .filter_map(|res| res.ok())
+        .filter(|line| line == "stop")
+        .next();
+    server_handler.stop();
+}
+
+struct ServerHandler {
+    addr: Addr<Server>,
+}
+
+impl ServerHandler {
+    fn start_server(state: CounterState) -> Self {
+        let (sender, receiver) = sync_channel::<Addr<Server>>(1);
+        thread::spawn(move || {
+            let system = actix::System::new("actix system");
+            let server_addr = server::new(move || App::with_state(state.clone())
+                .prefix("api")
+                .scope("v1", |scope| scope
+                    .resource("/next-update", |r| r.get().with(next_update_v1))
+                    .resource("/restart-node", |r| r.get().with(restart_node_v1))
+                    .resource("/counter", |r| r.get().with(counter_v1))
+                ))
+                .system_exit()
+                .bind("127.0.0.1:8088")
+                .unwrap()
+                .start();
+            sender.send(server_addr)
+                .unwrap();
+            system.run();
+        });
+        let addr = receiver.recv()
+            .unwrap();
+        ServerHandler { addr }
+    }
+
+    fn stop(self) {
+        self.addr.send(StopServer { graceful: true })
+            .wait()
+            .unwrap()
+            .unwrap();
     }
 }
 
-fn start_server() -> Addr<Server> {
-    let (sender, receiver) = sync_channel::<Addr<Server>>(1);
-    thread::spawn(move || {
-        let system = actix::System::new("no idea what's the point");
-        let server_addr = server::new(|| App::new()
-            .prefix("api")
-            .scope("v1", |scope| scope
-                .resource("/next-update", |r| r.get().f(next_update_v1))
-                .resource("/restart-node", |r| r.get().with(restart_node_v1))
-            ))
-            .system_exit()
-            .bind("127.0.0.1:8088")
-            .unwrap()
-            .start();
-        sender.send(server_addr)
-            .unwrap();
-        system.run();
-    });
-    receiver.recv()
-        .unwrap()
-}
-
-fn next_update_v1(_: &HttpRequest) -> impl Responder {
+fn next_update_v1(_: ()) -> impl Responder {
     Json(json!({
           "data": {
             "applicationName": "string",
@@ -69,4 +87,29 @@ struct RestartNodeV1Params {
 fn restart_node_v1(params: Query<RestartNodeV1Params>) -> impl Responder {
     println!("Restart! force_ntp_check = {}", params.force_ntp_check);
     HttpResponse::Ok()
+}
+
+struct CounterFuture {
+    state: CounterState,
+}
+
+impl Future for CounterFuture {
+    type Item = String;
+    type Error = ActixError;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let mut counter = self.state.lock()
+            .unwrap_or_else(|e| e.into_inner());
+        *counter += 1;
+        let message = format!("Call no. {}", counter);
+        println!("{}", message);
+        Ok(Async::Ready(message))
+    }
+}
+
+fn counter_v1(state: State<CounterState>) -> impl Responder {
+    let future = CounterFuture {
+        state: state.clone(),
+    };
+    Box::new(future) as FutureResponse<_>
 }
